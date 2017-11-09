42d700318ad6e2450a9a1b2417dfe87663176071
Wilber <wmartin8@gmail.com>
2017-11-08 20:30:45 -0500

    final touches

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -6,11 +6,11 @@ interface Abstract{
 const snippets: Abstract[] =[
     {
         title:"What I Do",
-        abstract:"Have worked with many tools across different industries facing many problems. I like to learn about software, new paradigms, and optimization techniques, but what drives me the most is solving tough problems."
+        abstract:"I have worked with many tools across different industries facing many problems. I like to learn about software, new paradigms, and optimization techniques, but what drives me the most is solving tough problems."
     },
     {
         title:"Achievements",
-        abstract:"Designed project architecture, database structures, mid tier APIs, front end javascript code, and accompanying internal tools, for varying companies, from chemical manufacturing and ad tech, to business intelligence products."
+        abstract:"I have designed project architectures, database structures, mid tier APIs, front-end javascript code and accompanying internal tools for varying companies, from chemical manufacturing and ad tech, to business intelligence products."
     },
     {
         title:"Skills",
