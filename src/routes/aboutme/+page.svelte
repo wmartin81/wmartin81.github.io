97ac16d3985fcfc060902fe3a3e96833e25f1d77
wilber martinez <wmartin8@gmail.com>
2025-07-27 00:18:57 -0400

    add prettier

--- src/routes/aboutme/+page.svelte
+++ src/routes/aboutme/+page.svelte
@@ -1,17 +1,20 @@
 <script>
 	const snippets = [
 		{
-			title: "What I Do",
-			abstract: "I have worked with many tools across different industries facing many problems. I like to learn about software, new paradigms, and optimization techniques, but what drives me the most is solving tough problems.",
+			title: 'What I Do',
+			abstract:
+				'I have worked with many tools across different industries facing many problems. I like to learn about software, new paradigms, and optimization techniques, but what drives me the most is solving tough problems.'
 		},
 		{
-			title: "Achievements",
-			abstract: "I have designed project architectures, database structures, mid tier APIs, front-end javascript code and accompanying internal tools for varying companies, from chemical manufacturing and ad tech, to business intelligence products.",
+			title: 'Achievements',
+			abstract:
+				'I have designed project architectures, database structures, mid tier APIs, front-end javascript code and accompanying internal tools for varying companies, from chemical manufacturing and ad tech, to business intelligence products.'
 		},
 		{
-			title: "Skills",
-			abstract: "Mostly C# and pure JavaScript, but with a sprinkle of Angular, React, TypeScript, Node, Webpack, Web Services, .NET Core, SQL, and Mongo (Phew that's alot!).",
-		},
+			title: 'Skills',
+			abstract:
+				"Mostly C# and pure JavaScript, but with a sprinkle of Angular, React, TypeScript, Node, Webpack, Web Services, .NET Core, SQL, and Mongo (Phew that's alot!)."
+		}
 	];
 </script>
 
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,33 +1,33 @@
 <script>
 	const experiences = [
 		{
-			company: "MediaRadar Inc.",
-			specialty: "Ad sales intelligence/Advertising Analysis",
-			title: "Senior Software Engineer",
+			company: 'MediaRadar Inc.',
+			specialty: 'Ad sales intelligence/Advertising Analysis',
+			title: 'Senior Software Engineer',
 			responsibilities:
-				"Extend and improve MediaRadar's binary gathering applications, supporting services, and database procedures. Oversee upgrade of internal tools portal frontend, from angular 1.5 to angular 15. Architect series of services to consume Large Datasets from 3rd parties (up to 10mil rows/day). Package and deploy release builds to remote servers. ",
+				"Extend and improve MediaRadar's binary gathering applications, supporting services, and database procedures. Oversee upgrade of internal tools portal frontend, from angular 1.5 to angular 15. Architect series of services across Azure/AWS to consume Large Datasets from 3rd parties (up to 10mil rows/day). Package and deploy release builds to remote servers. "
 		},
 		{
-			company: "Infragistics",
-			specialty: "Web Controls/UX/Data Analytics",
-			title: "Software Developer",
+			company: 'Infragistics',
+			specialty: 'Web Controls/UX/Data Analytics',
+			title: 'Software Developer',
 			responsibilities:
-				"Research/design/develop new features across all tiers for Infragistics' ReportPlus Server, an enterprise-level server application designed for storing, viewing and sharing business analytics. Migrate application front-end from JavaScript & JQuery to Angular & TypeScript front-end. Implement OAuth based data-sources. Add recent and favorite dashboard cross-platform sync functionality. Package and deploy release build to remote servers.",
+				"Research/design/develop new features across all tiers for Infragistics' ReportPlus Server, an enterprise-level server application designed for storing, viewing and sharing business analytics. Migrate application front-end from JavaScript & JQuery to Angular & TypeScript front-end. Implement OAuth based data-sources. Add recent and favorite dashboard cross-platform sync functionality. Package and deploy release build to remote servers."
 		},
 		{
-			company: "Princing Engine Inc.",
-			specialty: "Digital Marketing",
-			title: "Software Engineer",
+			company: 'Princing Engine Inc.',
+			specialty: 'Digital Marketing',
+			title: 'Software Engineer',
 			responsibilities:
-				"Developed/maintained/deployed digital marketing SaaS platform that would be hosted in AWS EC2 instance. Upgraded and maintained support systems and APIs. Led overhaul of MVC application for Wix marketplace.",
+				'Developed/maintained/deployed digital marketing SaaS platform that would be hosted in AWS EC2 instance. Upgraded and maintained support systems and APIs. Led overhaul of MVC application for Wix marketplace.'
 		},
 		{
-			company: "Ungerer & Company",
-			specialty: "Chemicals Manufacturer",
-			title: "Jr. Software Developer",
+			company: 'Ungerer & Company',
+			specialty: 'Chemicals Manufacturer',
+			title: 'Jr. Software Developer',
 			responsibilities:
-				"Supported, maintained, and extended, Expense System, PTO Application, Formula Inventory Tracking and Labeling, ERP system built on proprietary DML, SSRS Reports and other internal business applications.",
-		},
+				'Supported, maintained, and extended, Expense System, PTO Application, Formula Inventory Tracking and Labeling, ERP system built on proprietary DML, SSRS Reports and other internal business applications.'
+		}
 	];
 </script>
 
