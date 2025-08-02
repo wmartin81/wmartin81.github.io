eafd948cdbbe09d65ba8b571fefb4eafc68ef56e
Wilber Martinez <wilber@wmartin8@gmail.com>
2025-08-02 14:58:18 -0400

    make dedicated pages for contact and repo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,8 @@
 	<a href="/" class="nav-button home-button">Home</a>
 	<a href="/aboutme" class="nav-button aboutme-button">About Me</a>
 	<a href="/resume" class="nav-button resume-button">Resume</a>
-	<a href="https://github.com/wmartin81" class="nav-button repo-button">Repo</a>
-	<a href="https://www.linkedin.com/in/wilberm" class="nav-button contact-button">Contact</a>
+	<a href="/github" class="nav-button repo-button">Repo</a>
+	<a href="/linkedin" class="nav-button contact-button">Contact</a>
 </div>
 
 <div class="content">
---
+++ src/routes/github/+page.svelte
@@ -0,0 +1,8 @@
+<div>
+	<p>Checkout my github repo below.</p>
+	<p>I tinker with it once in while.</p>
+	<a href="https://github.com/wmartin81" class="link-item">
+		<div class="link-image repo-image"></div>
+		<span class="link">@wmartin81</span>
+	</a>
+</div>
