5b2faa4a0edfcad4a7a7e1b71f77ea056d9804fa
Wilber Martinez <wilber@wmartin8@gmail.com>
2025-08-02 15:15:59 -0400

    fix broken link

--- src/routes/linkedin/+page.svelte
+++ src/routes/linkedin/+page.svelte
@@ -1,6 +1,6 @@
 <div>
 	<p>Get in touch! See my LinkedIn profile below!</p>
-	<a href="https://www.linkedin.com/in/wilber" class="link-item">
+	<a href="https://www.linkedin.com/in/wilberm" class="link-item">
 		<div class="link-image contact-image"></div>
 		<span class="link">@wilberm</span>
 	</a>
